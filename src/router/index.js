b4c3e32caa167b34f03e80ab47761a381b236841
XIEMI2 <mike.xie@oocl.com>
2018-07-15 18:01:49 +0800

    Introduce Kanban component

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Retro from '@/components/retro/page/Retro'
+import dragKanban from '@/components/retro/page/dragKanban'
 
 Vue.use(Router)
 
@@ -10,6 +11,11 @@ export default new Router({
       path: '/retro',
       name: 'retro',
       component: Retro
+    },
+    {
+      path: '/dragKanban',
+      name: 'dragKanban',
+      component: dragKanban
     }
   ]
 })
