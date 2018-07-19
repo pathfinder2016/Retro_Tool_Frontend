809be7afb8a4919cff2380edfcafe1c2a3886cc0
XIEMI2 <mike.xie@oocl.com>
2018-07-19 20:41:25 +0800

    Fixed websocket problem

--- src/components/retro/page/Retro.vue
+++ src/components/retro/page/Retro.vue
@@ -284,16 +284,14 @@
           //连接关闭的回调方法
           this.socket.onclose = function () {
             console.log("I am closing")
+            _this.socket.close();
 
-            //监听窗口关闭事件，当窗口关闭时，主动去关闭websocket连接，防止连接还没断开就关闭窗口，server端会抛异常。
-            window.onbeforeunload = function () {
-              _this.socket.close();
-            }
-          };
-            // 路由跳转时结束websocket链接
-            this.$router.afterEach(function () {
-              _this.socket.close()
-            })
+          }
+
+          this.socket.onerror = function () {
+            _this.socket.close()
+          }
+
         }
       }
     },
